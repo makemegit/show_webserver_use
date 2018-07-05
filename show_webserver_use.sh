e1a01f085b9378b8bae6cd8ccf4c2ef1b303b618
makemegit <vechrogaz@gmail.com>
2018-07-05 11:15:42 +0300

    Update show_webserver_use.sh

--- show_webserver_use.sh
+++ show_webserver_use.sh
@@ -2,6 +2,8 @@ w | head -n1
 echo "---"
 echo "Connections ont port 80:"
 netstat -anp |grep 80 |wc -l
+echo "Connections ont port 443:"
+netstat -anp |grep 443 |wc -l
 echo "---"
 echo "Most hits from ApacheStatus:"
 lynx -dump -width 500 http://127.0.0.1/whm-server-status | awk '{print $11}' | sort | uniq -c | sort -n | tail -n10
