48d4cab5041faad9f22c74b35f09363f490774e6
makemegit <vechrogaz@gmail.com>
2019-01-08 15:47:15 +0200

    Update show_webserver_use.sh

--- show_webserver_use.sh
+++ show_webserver_use.sh
@@ -9,8 +9,10 @@ netstat -anp |grep 80 |wc -l
 echo "Connections on port 443:"
 netstat -anp |grep 443 |wc -l
 echo "---"
-echo "Most hits from ApacheStatus:"
+echo "Most IP hits from ApacheStatus:"
 lynx -dump -width 500 http://127.0.0.1/whm-server-status | awk '{print $12}' | sort | uniq -c | sort -n | tail -n10
+echo "Most WebSite hits from ApacheStatus:"
+lynx -dump -width 500 http://127.0.0.1/whm-server-status | awk '{print $14}' | sort | uniq -c | sort -n | tail -n10
 echo "---"
 echo "Mysql use:"
 mysqladmin processlist
